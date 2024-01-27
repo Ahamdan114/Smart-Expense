822106778d825160f44c750821cc8b7eecd6b672
Ahmed-Iulian Hamdan <hamdan.ahmed002@gmail.com>
2024-01-27 22:41:08 +0200

    Modified conditional rendering for shadow and scrollbar in TransactionList component

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,19 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "../context/GlobalState";
 import Transaction from "./Transaction";
 
 export default function TransactionList() {
     const { transactions } = useContext(GlobalContext);
+    const [hasShadow, setHasShadow] = useState(false);
+    function verifyHistory() {
+        if (transactions.length >= 4) setHasShadow(true);
+        else setHasShadow(false);
+    }
+    
+    useEffect(() => {
+        verifyHistory();
+    }, [transactions]);
 
     return (
         <>
-            <h3 className="history-title">History</h3>
+            <h3 className={hasShadow ? "history-title" : ""} style={{}}>
+                History
+            </h3>
             <ul id="list" className="list history-review">
                 {transactions.map((transaction) => (
-                    <Transaction
-                        key={transaction.id}
-                        transaction={transaction}
-                    />
+                    <Transaction key={transaction.id} transaction={transaction} />
                 ))}
             </ul>
         </>
