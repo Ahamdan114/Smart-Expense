31a1c7c94ce4a51aa259482a0cc6b8d14ed7fc8f
Ahmed-Iulian Hamdan <hamdan.ahmed002@gmail.com>
2024-01-28 12:55:35 +0200

    Added implementation for case where user wants to press the insertion in order to delete it.

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -13,7 +13,7 @@ export default function Transaction({ transaction }) {
     const sign = transaction.amount > 0 ? "+" : "-";
 
     return (
-        <li
+        <li style={{width: isDelete ? "85%" : "95%"}}
             className={`item-container ${transaction.amount < 0 ? "minus" : "plus"}`}
             onClick={handleClick}
         >
