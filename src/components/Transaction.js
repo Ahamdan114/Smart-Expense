b9121ffb6c53808748acd69fe8fc2c12367237a2
Ahmed-Iulian Hamdan <hamdan.ahmed002@gmail.com>
2024-01-28 12:50:31 +0200

    Added click animation for deletion

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,19 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { GlobalContext } from "../context/GlobalState";
 
 export default function Transaction({ transaction }) {
     const { deleteTransaction } = useContext(GlobalContext);
+    const [isDelete, setIsDelete] = useState(false);
+    const handleClick = () => {
+        setTimeout(() => {
+            setIsDelete(false);
+        }, 5000);
+        setIsDelete(true);
+    };
     const sign = transaction.amount > 0 ? "+" : "-";
 
     return (
-        <li className={`item-container ${transaction.amount < 0 ? "minus" : "plus"}`}>
-            {transaction.text}{" "}
+        <li
+            className={`item-container ${transaction.amount < 0 ? "minus" : "plus"}`}
+            onClick={handleClick}
+        >
+            {transaction.text}
             <div>
                 {sign}${Math.abs(transaction.amount)}
             </div>
             <button
                 onClick={() => deleteTransaction(transaction.id)}
-                className="delete-btn"
+                className="delete-btn" style={{opacity: isDelete ? 1 : 0}}
             >
                 X
             </button>
